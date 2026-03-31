af2418f3da4090df815f0a7e4ffe1fabc332a4b0
Tekfred <asagodfred101@gmail.com>
2026-03-31 14:13:26 +0000

    testing basic Dart programming

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  static const String _title = 'Flutter Stateful Clicker Counter';
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: _title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.green),
+        // useMaterial3: false,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo RasPerry Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+  const MyHomePage({super.key});
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  // This class is the configuration for the state.
+  
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState(
+  );
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String name = 'sugatiti';
+  String favFood = "Waakye";
   int _counter = 0;
+
+  int addNumbers(int a, int b){
+    return a + b;
+  }
+
+  int multiply(int a, int b){
+    return a * b;
+  }
+
+  void changeName(){
+    setState(() {
+      name = 'Flutter pro 🚀';
+    });
+  }
+
+
 
   void _incrementCounter() {
     setState(() {
@@ -77,13 +74,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Flutter Demo Click Counter'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -98,25 +91,43 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'I am learning flutter like a pro',
+              
+            
+            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '$_counter', 
+              style: const TextStyle(fontSize: 25),
+            ),
+            Text (
+              'Hello %name',
+            ),
+            Text(
+              "Favorite food is $favFood",
+            ),
+            Text(
+              "2 * 3 = ${multiply(2, 3)}",
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: Column(
+  mainAxisAlignment: MainAxisAlignment.end,
+  children: [
+    FloatingActionButton(
+      onPressed: _incrementCounter,
+      child: const Icon(Icons.add),
+    ),
+    const SizedBox(height: 10),
+    FloatingActionButton(
+      onPressed: changeName,
+      child: const Icon(Icons.edit),
+    ),
+  ],
+),
     );
   }
 }
